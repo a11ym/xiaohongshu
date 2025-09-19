a8342e5379d206cfbec00f603f69c3d7b0ef8c3f
a11ym <2868776500@qq.com>
2025-09-20 01:49:21 +0800

    feat: 添加iOS和Android平台的启动屏实现代码 更新相关配置文件以支持启动屏功能

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import { StatusBar } from 'react-native';
 import { useTheme } from './src/hooks/useTheme';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider } from './src/contexts/AuthContext';
+import BootSplash from 'react-native-bootsplash';
 import React from 'react';
 const linking = {
   prefixes: [],
@@ -23,12 +24,25 @@ export default function App() {
   const navigationRef = useNavigationContainerRef();
   // console.log("🚀 ~ App ~ navigationRef:", navigationRef)
   const { isDarkMode } = useTheme();
+  // useEffect(() => {
+  //   const init = async () => {
+  //     // …do multiple sync or async tasks
+  //   };
+
+  //   init().finally(async () => {
+  //     await BootSplash.hide({ fade: true });
+  //     console.log("BootSplash has been hidden successfully");
+  //   });
+  // }, []);
 
   return (
     <GestureHandlerRootView>
       <SafeAreaProvider>
         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
         <NavigationContainer
+          onReady={() => {
+            BootSplash.hide();
+          }}
           ref={navigationRef}
           linking={linking}>
           <AuthProvider>
